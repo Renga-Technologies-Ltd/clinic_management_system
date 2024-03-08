ab62e43c15bab86169dcb1fc485e3c0b4d5f3df9
Collins Kandie <collinskandie1@gmail.com>
2024-03-08 16:23:12 +0300

    Bubble.io

--- Backend/app.js
+++ Backend/app.js
@@ -1,25 +1,21 @@
-var createError = require("http-errors");
+
 var express = require("express");
-var path = require("path");
+
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const cors = require("cors");
+
 var indexRouter = require("./routes/index");
 var app = express();
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
 require("dotenv").config();
-// connect to db and start server
 require("./schemas/db");
-
-console.log("Booting....");
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// redirect all api calls to index router
+console.log("Booting...");
 app.use("/api", indexRouter);
 
 // run server
