19d1e313c00f5fe9720b0e881f137738b8e36632
Collins Kandie <tn.masroor@gmail.com>
2024-03-21 15:12:09 +0300

    dd

--- frontend/src/views/app-views/pages/invoice/index.js
+++ frontend/src/views/app-views/pages/invoice/index.js
@@ -25,23 +25,28 @@ export class Invoice extends Component {
               <address>
                 <p>
                   <span className="font-weight-semibold text-dark font-size-md">
-                    CMS, Inc.
+                    Mahapatra Medi-Care Limited
                   </span>
                   <br />
-                  <span>00100 Nairobi</span>
+                  <span>6th Floor; B Wing, Doctor's Park</span>
+                  <br />                 
+                  <span>3rd Parklands Avenue</span>
+                  <br />                
+                  <span>P.O Box: 38158 -00628</span>
                   <br />
                   <span>Nairobi, Kenya</span>
                   <br />
+                 
                   <abbr className="text-dark" title="Phone">
                     Phone:
                   </abbr>
-                  <span>(254) 722545735</span>
+                  <span> (254)743349929</span>
                 </p>
               </address>
             </div>
             <div className="mt-3 text-right">
               <h2 className="mb-1 font-weight-semibold">Invoice #9972</h2>
-              <p>16/01/204</p>
+              <p>16/01/2024</p>
               <address>
                 <p>
                   <span className="font-weight-semibold text-dark font-size-md">
@@ -99,12 +104,12 @@ export class Invoice extends Component {
                       thousandSeparator={true}
                     />
                   </p>
-                  <p>
+                  {/* <p>
                     vat (16%) :{" "}
                     {(
                       Math.round((this.total() / 100) * 16 * 100) / 100
                     ).toFixed(2)}
-                  </p>
+                  </p> */}
                 </div>
                 <h2 className="font-weight-semibold mt-3">
                   <p>Prices are VAT inclusive </p>
@@ -120,7 +125,7 @@ export class Invoice extends Component {
               </div>
             </div>
             <p className="mt-5">
-              <small>
+              {/* <small>
                 In exceptional circumstances, Financial Services can provide an
                 urgent manually processed special cheque. Note, however, that
                 urgent special cheques should be requested only on an emergency
@@ -128,7 +133,7 @@ export class Invoice extends Component {
                 and considerable staff resources. Requests need to be supported
                 by a letter explaining the circumstances to justify the special
                 cheque payment
-              </small>
+              </small> */}
             </p>
           </div>
           <hr className="d-print-none" />
